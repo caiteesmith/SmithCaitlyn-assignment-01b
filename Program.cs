e4612858e30232d795bf10a895b931d716e6b6ba
caiteesmith <caiteee@gmail.com>
2023-01-24 19:37:39 -0500

    commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+/* PROGRAM DESCRIPTION
+Programmer: Caitlyn Smith
+Date: 01/18/2023
+Purpose: Assignment 01B: Write a 'Hello World' program */
+
+using System;
+using static System.Console;
+
+namespace HelloWorldProg {
+    class HelloWorld {
+        static void Main() {
+            WriteLine("Hello, World! I am Caitlyn Smith.");
+        }
+    }
+}
